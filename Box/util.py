4bcaafbe467ed4d2c3ff99c31233425f8c5a63d8
Sebastian <seba@cloudnative.co.jp>
2020-04-27 04:43:55 +0000

    add get_arguments in Box/util.py

--- Box/util.py
+++ Box/util.py
@@ -37,3 +37,47 @@ def webhook_response(payload: dict, status=200):
         'headers': {'content-type': 'application/json'},
         'body': message
     }
+
+HEADER_KEYS = [
+    "if_match",
+    "if_none_match",
+    "x_rep_hints",
+    "content_md5",
+    "content_range",
+    "content_type",
+    "content_length",
+    "digest",
+    "range"
+]
+
+def get_arguments(args: dict, keys: list = None, ignores: list = None):
+    def snake_to_kebab(key):
+        ret = []
+        for key in key.split("_"):
+            ret.append(key.capitalize())
+        return "-".join(ret)
+
+    ret = {}
+    for ky in args:
+        if ky == "self":
+            continue
+        if keys is not None:
+            if ky not in keys:
+                continue
+        if ignores is not None:
+            if ky in ignores:
+                continue
+        if args[ky] is None:
+            continue
+        if args[ky] is not None:
+            if isinstance(args[ky], datetime.datetime):
+                ret[ky] = args[ky].strftime("%Y-%m-%dT%H:%M:%S%z")
+            elif isinstance(args[ky], list):
+                ret[ky] = ",".join(args[ky])
+            else:
+                ret[ky] = args[ky]
+        if ky in HEADER_KEYS:
+            v = ret.pop(ky)
+            ky = snake_to_kebab(ky)
+            ret[ky] = v
+    return ret
